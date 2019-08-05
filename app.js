8c813128024ca707122875c2a3e1bd4a9398d239
Zach Alcerro <zachalcerro@gmail.com>
2019-08-05 21:57:42 +0900

    adds alert js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello, world');
+const alert = document.getElementById("alert");
+
+
+
+
+
+// Alert Banner
+alert.innerHTML =
+`<div class="alert-banner">
+    <p><strong>Alert: </strong> You have <strong> 6 </strong> overdue tasks to complete.</p>
+    <p class="alert-banner-close">x</p>
+</div>
+`
+// Close Alert Banner
+alert.addEventListener('click', e => {
+    const element = e.target;
+    if(element.classList.contains("alert-banner-close")) {
+        alert.style.display = "none"
+    }
+})
